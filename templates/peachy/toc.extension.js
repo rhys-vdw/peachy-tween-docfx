34e6cf87bbde9c9f2def8174a3b40c89bbd147f1
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-07-22 22:54:24 +1000

    Disable side filter

--- templates/peachy/toc.extension.js
+++ templates/peachy/toc.extension.js
@@ -4,6 +4,7 @@
  * This method will be called at the start of exports.transform in toc.html.js
  */
 exports.preTransform = function (model) {
+  model._disableSideFilter = true;
   return model;
 }
 
